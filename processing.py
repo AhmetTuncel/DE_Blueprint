f78442d9793a38667eda46775439182809b0cb82
ahmettuncel <ahmet.tuncel@outlook.com>
2023-12-17 11:39:23 +0100

    A new feature has been added

--- processing.py
+++ processing.py
@@ -1,7 +1,10 @@
 import logging
 import sys
-from pyspark.sql import DataFrame, functions as F
+from itertools import chain
 
+from pyspark.sql import DataFrame, functions as F
+from pyspark.sql.functions import when, col, create_map, lit
+from pyspark.sql.types import IntegerType, StringType
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -42,14 +45,14 @@ class DomainTable(StagingTable):
         _df_stg_domain_2 = self.table_data["stg_domain_2"]
         _df_stg_domain_3 = self.table_data["stg_domain_3"]
         _df_stg_domain_4 = self.table_data["stg_domain_4"]
-
+        _df_stg_dim_domain_1 = self.table_data["stg_dim_domain_1"]
 
         logger.info(f"Initialize Data Sources")
 
-        #Keeping id column for joining operations
+        # Keeping id column for joining operations
         _df_stg_domains_combine = _df_stg_domain_1.withColumn("id", _df_stg_domain_2.id)
 
-        #adding deperse column (needed for cust implementation)
+        # adding deperse column (needed for cust implementation)
         _df_stg_domains_combine = self.concealing_helper.execute(
             self.spark,
             _df_stg_domains_combine,
@@ -69,13 +72,26 @@ class DomainTable(StagingTable):
 
         return _df_domains_combine
 
-
-    def add_assembly_attributes(_df_domains_combine: DataFrame, _df_stg_domain_4: DataFrame) -> DataFrame:
-
+    def add_assembly_attributes(_df_domains_combine: DataFrame, _df_stg_dim_domain_1: DataFrame) -> DataFrame:
         _df_domains_combine = _df_domains_combine.withColumn('col5', when(
             (_df_domains_combine['col6'].isNotNull() & (F.length('col7') > 3)),
             F.split(F.col("col8"), "-")[0].cast(F.StringType())
         ).otherwise(F.lit(None).cast(F.StringType())))
 
+        _df_domains_combine = (when(col('c_country') == 'BRA', '82.11')
+                               .when((col('c_country') == 'DE'), '31.42')
+                               .when((col('c_country') == 'TR'), '19.82')
+                               .when((col('c_country') == 'USA'), '10.00')
+                               .when((col('c_country') == 'UK'), '34.22')
+                               .otherwise('').cast(StringType()))
 
 
+        "Creating a dictionary named _df_stg_dim_domain_dic that maps the synthetic_col column"
+        _df_stg_dim_domain_dic = {row['synthetic_col']: row['col_1'] for row in _df_stg_dim_domain_1.collect()}
+        mapping_expr = create_map([lit(x) for x in chain(*_df_stg_dim_domain_dic.items())])
+
+        _df_domains_combine = _df_domains_combine.withColumn('col_6',
+                                                             mapping_expr[_df_domains_combine['col_7']].cast(
+                                                                 StringType()))
+
+        return _df_domains_combine
